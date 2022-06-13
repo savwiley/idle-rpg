d8a9c00c217b4da0485ded59cdb4401a9f31bf17
Savannaha <savwiley@yahoo.com>
2022-06-13 17:35:36 -0500

    fix grid overlap

--- src/components/Sidebar/styled.js
+++ src/components/Sidebar/styled.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 export const Box = styled.div`
   border-radius: 20px;
-  border: 5px double var(--text);
+  border: 6px double var(--text);
   width: 100%;
   height: 100%;
+  grid-area: 1 / 1 / 4 / 2;
 `;
