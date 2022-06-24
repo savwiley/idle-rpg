3da2a4c90b0668203ccb3a90e5985e10912ff86d
Savannaha <savwiley@yahoo.com>
2022-06-24 13:35:45 -0500

    change box design

--- src/components/Sidebar/styled.js
+++ src/components/Sidebar/styled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  border-radius: 20px;
-  border: 6px double var(--text);
+  border-radius: 10px;
+  border: 1px solid var(--border);
   width: 100%;
   height: 100%;
   grid-area: 1 / 1 / 4 / 2;
+  box-shadow: 0 0 4px rgba(0,0,0,0.8);
+  background: linear-gradient(150deg, var(--background), var(--background2));
 `;
