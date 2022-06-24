2cbc6c9695e593b218cce6b2227f14a1cfbf49ff
Savannaha <savwiley@yahoo.com>
2022-06-24 13:36:40 -0500

    change box & increment design

--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
 export const Box = styled.div`
-  border-radius: 20px;
-  border: 6px double var(--text);
+  border-radius: 10px;
+  border: 1px solid var(--border);
   width: 100%;
   height: 100%;
   grid-area: 2 / 2 / 4 / 4;
+  box-shadow: 0 0 4px rgba(0,0,0,0.8);
+  background: linear-gradient(150deg, var(--background), var(--background2));
+  overflow: hidden;
+`;
+
+export const Numbers = styled.div`
+  background: linear-gradient(90deg, var(--gradient), var(--gradient2));
+  text-align: center;
+  font-size: 30px;
+  padding: 10px 0;
+  border-bottom: 1px solid var(--border);
+  cursor: default;
+  box-shadow: 0 5px 4px rgba(0,0,0,0.3);
 `;
